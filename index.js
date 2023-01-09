66231a6f46c9611915653cb5e156f2ce74ecc97f
riki <riki.hoshino.ejy@gmail.com>
2023-01-09 17:15:18 +0900

    new

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
 const mongoose = require("mongoose");
 require('dotenv').config();
-
 
 mongoose.set('strictQuery', false)
 mongoose.connect(process.env.MONGO_URL, {
